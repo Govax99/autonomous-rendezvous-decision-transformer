18dc2edc56b20d00ff76f049bbf82e1e3b53ac02
Davide Zamblera <davide.zamble@gmail.com>
2023-07-01 10:38:20 +0200

    Minor changes -added max computation time -added addition of necessary folder paths -added option for result folder

--- optimal-control/create_database.m
+++ optimal-control/create_database.m
@@ -4,18 +4,26 @@ function [] = create_database(varargin)
 % https://github.com/yoptimization/yop/archive/refs/tags/v1.0-rc3.zip
 % usage example:
 % !matlab -nodesktop -nosplash -r "create_database -ntot 2 -nsave 1 -printlevel 2"
+addpath(genpath('casadi'));
+addpath(genpath('yop-1.0-rc3'));
 
 % 1) deal with options
 optionsNames = string(varargin(1:2:end));
-optionsValues = string(varargin(2:2:end));
-optionsValues = str2double(optionsValues);
+optionsValues = varargin(2:2:end);
+for k = 1:length(optionsValues)
+    tmp = str2double(optionsValues(k));
+    if (~isnan(tmp))
+        optionsValues{k} = tmp;
+    end
+end
 
 defaultOptionsValues = set_options(optionsNames,optionsValues);
 
-Ntot = defaultOptionsValues(1);
-Nsave = defaultOptionsValues(2);
+Ntot = defaultOptionsValues{1};
+Nsave = defaultOptionsValues{2};
 Nbatches = floor(Ntot/Nsave);
-printLevel = defaultOptionsValues(3);
+printLevel = defaultOptionsValues{3};
+saveDir = defaultOptionsValues{4};
 
 % 2) set options and parameters
 
@@ -23,31 +31,31 @@ printLevel = defaultOptionsValues(3);
 rng(42);
 
 % ----- PARAMETERS FOR DYNAMIC SIMULATION ----- %
-LC = 1; % length side chaser box
+LC = 1.5; % length side chaser box
 LT = 3; % length side target box
-parameters.J_C = eye(3);
-parameters.J_T = eye(3);
+parameters.J_C = diag([2/3, 2/5, 2/3]);
+parameters.J_T = diag([11/3, 3, 11/3]);
 parameters.m_C = 1;
-parameters.OM = 0.005;
+parameters.OM = 7.272e-5;
 parameters.pberth = [5 0 0]';
 parameters.kP_tr = 0.1*eye(3);
 parameters.kD_tr = 1*eye(3);
 parameters.kP_rot = 1*eye(3);
 parameters.u_lim = ones(1,6);
-parameters.r2 = LT^2; % keep out zone
+parameters.r2 = 4^2; % keep out zone
 
 % ----- META-PARAMETERS: OPTIONS FOR SOLVERS AND REWARD DEFINITION ----- %
 options.K_action = eye(6);
-options.R_success = 5;
+options.R_success = 25;
 options.R_collision = -10;
 options.R_timeout = -5;
 
-options.xpos_lim = [-15 15];
-options.ypos_lim = [-15 15];
-options.zpos_lim = [-15 15];
-options.wx_lim = [-0.3 0.3];
-options.wy_lim = [-0.3 0.3];
-options.wz_lim = [-0.3 0.3];
+options.xpos_lim = [-30 30];
+options.ypos_lim = [-30 30];
+options.zpos_lim = [-30 30];
+options.wx_lim = [-0.05 0.05];
+options.wy_lim = [-0.05 0.05];
+options.wz_lim = [-0.05 0.05];
 options.qaxis_lim = [-1 1; ...
                      -1 1; ...
                      -1 1];
@@ -60,7 +68,7 @@ tproc = zeros(1,Nsave);
 results = zeros(1,Nsave);
 infos = strings(1,Nsave);
 db(1:Nsave) = struct("objective",[],"success",[],"time",[],"observation",[],"action",[],"done",[],"reward",[]);
-mkdir('results')
+mkdir(saveDir)
 for k = 1:Nbatches
     parfor (i = 1:Nsave,8)
         [tj, tsolve] = database.generate_trajectory(options, parameters);
@@ -71,7 +79,7 @@ for k = 1:Nbatches
     end
     visualization.report_database_generation(k,infos,tproc,results,printLevel)
 
-    save("results/database_batch_" + string(k) + ".mat","db")
+    save(saveDir + "/database_batch_" + string(k) + ".mat","db")
     tproc = zeros(1,Nsave);
     results = zeros(1,Nsave);
     infos = strings(1,Nsave);
